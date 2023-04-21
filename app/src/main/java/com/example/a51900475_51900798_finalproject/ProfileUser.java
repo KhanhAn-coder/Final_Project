8f13ea5ab3e9a8e8405cc1622638ea0b6ce62ffc
Nguyen Van Khanh An <51900475@student.tdtu.edu.vn>
2023-04-21 16:43:20 +0700

    Update Final_Project

---
+++ app/src/main/java/com/example/a51900475_51900798_finalproject/ProfileUser.java
@@ -0,0 +1,31 @@
+package com.example.a51900475_51900798_finalproject;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+public class ProfileUser extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile_user);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_bar,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/com/example/a51900475_51900798_finalproject/SignIn.java
@@ -0,0 +1,14 @@
+package com.example.a51900475_51900798_finalproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SignIn extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+    }
+}
