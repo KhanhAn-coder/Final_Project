c5fe73192c10e8608e5dcdfb1945dea7000107f3
Huynh Gia Huy <51900798@student.tdtu.edu.vn>
2023-05-05 16:44:01 +0700

    chỉnh giao diện

--- app/src/main/java/com/example/a51900475_51900798_finalproject/ProfileUser.java
+++ app/src/main/java/com/example/a51900475_51900798_finalproject/ProfileUser.java
@@ -10,19 +10,30 @@ import android.app.SearchManager;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 
 import product.Product;
 import product.ProductAdapter;
+import product.ProductViewHolder;
+import product.Productss;
 
 public class ProfileUser extends AppCompatActivity {
     ArrayList<String> listUserExtensions = new ArrayList<>();
@@ -31,6 +42,7 @@ public class ProfileUser extends AppCompatActivity {
     RecyclerView profileUserHotSalesRV;
     ProfileUserExtensionsAdapter profileUserExtensionsAdapter;
     BottomNavigationView bottomNavigationUserProfile;
+    DatabaseReference RootRef;
 
     ProductAdapter productAdapter;
     TextView tvHotSales, profile_tv_username;
@@ -56,8 +68,49 @@ public class ProfileUser extends AppCompatActivity {
 
 
 
-
+        RootRef = FirebaseDatabase.getInstance().getReference("Products");
         profileUserHotSalesRV = findViewById(R.id.profileUserHotSalesRV);
+
+        profileUserHotSalesRV.setHasFixedSize(true);
+        profileUserHotSalesRV.setLayoutManager(new LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false));
+        Query query = RootRef.orderByChild("type").equalTo("sales_product");
+        FirebaseRecyclerOptions<Productss> options = new FirebaseRecyclerOptions.Builder<Productss>()
+                .setQuery(query,Productss.class)
+                .build();
+
+        FirebaseRecyclerAdapter<Productss, ProductViewHolder> adapter = new FirebaseRecyclerAdapter<Productss, ProductViewHolder>(options) {
+            @Override
+            protected void onBindViewHolder(ProductViewHolder holder, int position, Productss model) {
+
+                holder.textView_title.setText(model.getTitle());
+                holder.textView_price.setText(model.getPrice()+"đ");
+                holder.textView_rating.setText(String.valueOf(model.getRating()));
+                holder.textView_sold.setText(String.valueOf(model.getSold())+"k");
+                Picasso.get().load(model.getSourceID()).into(holder.imageView_item);
+
+                holder.itemView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Intent intent = new Intent(ProfileUser.this,ProuctDetails.class);
+                        Bundle extras = new Bundle();
+                        extras.putString("productID",model.getProductID());
+                        extras.putString("type",model.getType());
+                        intent.putExtras(extras);
+                        startActivity(intent);
+                    }
+                });
+
+            }
+
+            @Override
+            public ProductViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+                View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.product_item,parent,false);
+                ProductViewHolder holder = new ProductViewHolder(view);
+                return holder;
+            }
+        };
+        profileUserHotSalesRV.setAdapter(adapter);
+        adapter.startListening();
 
 
 
